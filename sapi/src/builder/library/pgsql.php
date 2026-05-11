97a317ce1d87dc37b7263805c1376fc497d9f84c
jingjingxyk <zonghengbaihe521@qq.com>
2026-05-11 21:33:25 +0800

    升级 libpq 版本

--- sapi/src/builder/library/pgsql.php
+++ sapi/src/builder/library/pgsql.php
@@ -18,14 +18,16 @@ return function (Preprocessor $p) {
         (new Library('pgsql'))
             ->withHomePage('https://www.postgresql.org/')
             ->withLicense('https://www.postgresql.org/about/licence/', Library::LICENSE_SPEC)
-            ->withUrl('https://ftp.postgresql.org/pub/source/v16.3/postgresql-16.3.tar.gz')
-            ->withManual('https://www.postgresql.org/docs/current/install-procedure.html#CONFIGURE-OPTIONS')
+            //->withUrl('https://ftp.postgresql.org/pub/source/v16.3/postgresql-16.3.tar.gz')
+            ->withUrl('https://ftp.postgresql.org/pub/source/v18.3/postgresql-18.3.tar.gz')
             ->withManual('https://www.postgresql.org/docs/current/install-procedure.html#CONFIGURE-OPTIONS#:~:text=Client-only%20installation')
-            ->withFileHash('md5', '8a58db4009e1a50106c5e1a8c4b03bed')
+            //->withFileHash('md5', '8a58db4009e1a50106c5e1a8c4b03bed')
             ->withPrefix($pgsql_prefix)
+            ->withBuildCached(false)
+            ->withInstallCached(false)
             ->withBuildScript(
                 <<<EOF
-            {$custom_env_start}
+            # {$custom_env_start}
             test -d build && rm -rf build
             mkdir -p build
             cd build
@@ -34,10 +36,12 @@ return function (Preprocessor $p) {
 
             sed -i.backup "s/invokes exit\'; exit 1;/invokes exit\';/"  ../src/interfaces/libpq/Makefile
 
-            sed -i.backup "278 s/^/# /"  ../src/Makefile.shlib
-            sed -i.backup "402 s/^/# /"  ../src/Makefile.shlib
+            # postgresql 18 需要如下配置
+            sed -i.backup '/install-lib: install-lib-shared/s/^/# /'        ../src/Makefile.shlib
+            sed -i.backup '/\$(COMPILER) -dynamiclib -install_name/s/^/#/' ../src/Makefile.shlib
 
             PACKAGES="libssl libcrypto openssl zlib icu-uc icu-io icu-i18n readline libxml-2.0  libxslt libzstd liblz4"
+            CFLAGS="-DUSE_PRIVATE_ENCODING_FUNCS" \
             CPPFLAGS="$(pkg-config  --cflags-only-I --static \$PACKAGES )" \
             LDFLAGS="$(pkg-config   --libs-only-L   --static \$PACKAGES ) {$ldflags} " \
             LIBS="$(pkg-config      --libs-only-l   --static \$PACKAGES ) {$libs}  " \
@@ -60,9 +64,9 @@ return function (Preprocessor $p) {
             --without-bonjour \
             --without-tcl
 
-            make -C src/bin/pg_config install
+            make -C  src/bin/pg_config install
 
-            make -C src/include install
+            make -C  src/include install
 
             make -C  src/common install
 
@@ -70,7 +74,7 @@ return function (Preprocessor $p) {
 
             make -C  src/interfaces/libpq install
 
-            {$custom_env_end}
+            # {$custom_env_end}
 EOF
             )
             ->withScriptAfterInstall(
@@ -95,8 +99,8 @@ EOF
                 'liblz4'
             )
     );
-    $p->withExportVariable('LIBPQ_CFLAGS', '$(pkg-config  --cflags --static libpq)');
-    $p->withExportVariable('LIBPQ_LIBS', '$(pkg-config    --libs   --static libpq)');
+    $p->withExportVariable('LIBPQ_CFLAGS', '$(pkg-config  --cflags --static  libpq)');
+    $p->withExportVariable('LIBPQ_LIBS', '$(pkg-config    --libs   --static  libpq)');
     $p->withExportVariable('PGSQL_CFLAGS', '$(pkg-config  --cflags --static  libpq)');
     $p->withExportVariable('PGSQL_LIBS', '$(pkg-config    --libs   --static  libpq)');
 };
